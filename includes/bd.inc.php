01fe898b9d0ea5f7113c269ab4b46e78b5e75a17
OlegKamkin <oleg2014nike@mail.ru>
2022-12-05 22:25:52 +0300

    создал бд и запросы

---
+++ includes/bd.inc.php
@@ -0,0 +1,19 @@
+<?php
+    $serverName = "localhost";
+    $dBUserName = "root";
+    $dBPassword = "";
+    $dBName = "tp";
+    $dsn = "mysql:host=$serverName; dbname=$dBName; charset=utf8";
+    $opt = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ];
+    try {
+        $pdo = new PDO($dsn, $dBUserName, $dBPassword, $opt);
+    }
+    catch (PDOException $e) {
+        echo "Error!: .$e -> getMessage()";
+        die();
+    }
+?>
